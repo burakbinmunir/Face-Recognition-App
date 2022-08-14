38c2fb3e1921c3eaeb63af53cf0d5d625105ab6f
Burak Bin Munir <burakbinmunir@gmail.com>
2022-08-14 19:33:07 +0500

    up till now Added Api Api is working for now as am checking the output on console But there was a problem i was not able to set the state of imageUrl through input

--- src/App.js
+++ src/App.js
@@ -4,22 +4,37 @@ import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
 import React,{Component} from 'react';
+import Clarifai from 'clarifai';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
+
+const app = new Clarifai.App({
+  apiKey: '212662fbf67f42dab11937d7085644c3'
+});
 
 class App extends Component {
 
   constructor(){
     super();
     this.state = {
-      input:''
+      input:'',
+      imageLink: ''
     }
   }
 
   onInputSubmit =(event) =>{
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
-  onSubmitButton (){
-    console.log("Clicked!");
+  onSubmitButton = () => {
+
+    app.models.predict(Clarifai.COLOR_MODEL, this.state.input).then(
+      function(response){
+          console.log(response);
+      },
+      function(err){
+
+      }
+    );
   }
 
   render(){
@@ -29,6 +44,7 @@ class App extends Component {
           <Logo />
           <Rank/>
           <ImageLinkForm onInputSubmit={this.onInputSubmit} onSubmitButton={this.onSubmitButton}  />
+          <FaceRecognition imgUrl={this.state.imageLink }/>
         </div>
     );
   }
---
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FaceRecognition = ({imgUrl}) => {
+    return (
+        <div className="center">
+            <img alt="a picture" src={"https://samples.clarifai.com/face-det.jpg"}/>
+        </div>
+    );
+}
+
+export default FaceRecognition;
