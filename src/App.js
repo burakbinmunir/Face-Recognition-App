0e67e0cc4b0cb6f6170f28524bed7ae2df6f09ba
Burak Bin Munir <burakbinmunir@gmail.com>
2022-08-14 18:15:19 +0500

    Added some fuunctionality Submit Buttion adn Input feild are working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank/>
-      <ImageLinkForm/>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      input:''
+    }
+  }
+
+  onInputSubmit =(event) =>{
+    console.log(event.target.value);
+  }
+
+  onSubmitButton (){
+    console.log("Clicked!");
+  }
+
+  render(){
+    return (
+        <div className="App">
+          <Navigation />
+          <Logo />
+          <Rank/>
+          <ImageLinkForm onInputSubmit={this.onInputSubmit} onSubmitButton={this.onSubmitButton}  />
+        </div>
+    );
+  }
 }
 
 export default App;
