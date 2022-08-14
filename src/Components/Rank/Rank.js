1fb60e0015be352ecccafa6caad2c5e85eb7db08
Burak Bin Munir <burakbinmunir@gmail.com>
2022-08-14 18:01:03 +0500

    added some components

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Rank = () =>{
+    return (
+        <div className="white">
+            <h1>
+                Hey Mr.Burak you are ranked as
+            </h1>
+
+            <h2>
+                #1
+            </h2>
+        </div>
+    );
+}
+
+export default Rank;
